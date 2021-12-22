dc0c1ada97ae7ae1603288f445a63c5190b1c690
Spyware <gabrielluisdm13@gmail.com>
2021-12-21 21:05:48 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ verd = '\033[32;1m'
 amar = '\033[33;1m'
 azul = '\033[34;1m'
 n = '\033[37;1m'
-fi = '\033[m'
+f = '\033[m'
+l = '\033[4m'
 
-banner = f'{azul}{open("banner.txt").read()}{fi}'
+banner = open("banner.txt").read()
 
 def limp():
     system('cls||clear')
@@ -18,26 +19,50 @@ def ent():
     input('\nenter para continuar')
 
 def calcu(eq=''):
-    if 'x' in eq:
-        eq.replace('x', '*')
+    if 'x' in eq: eq = eq.replace('x', '*')
+    if ',' in eq: eq = eq.replace(',', '.')
+    if '^' in eq: eq = eq.replace('^', '**')
+    if '{' in eq: eq = eq.replace('{', '(')
+    if '}' in eq: eq = eq.replace('}', ')')
+    if '[' in eq: eq = eq.replace('[', '(')
+    if ']' in eq: eq = eq.replace(']', ')')
     return eval(eq)
 
+cores = verd, azul, amar
 while True:
-    cores = verd, azul, amar
-    cor = choice(cores)
     limp()
-    print(banner)
-    opc = input(f'{n}[ {cor}1 {n}] > {cor}calculadora\n{n}[ {cor}2 {n}] > {cor}operadores aritiméticos\n\n{n}[ {cor}0{n} ] > {cor}sair\n\n{cor}>>>{n} ').strip()[0]
+    cor = choice(cores)
+    print(cor+banner+f)
+    try:
+        opc = input(f'{n}[ {cor}1 {n}] > {cor}calculadora\n{n}[ {cor}2 {n}] > {cor}operadores aritiméticos\n\n{n}[ {cor}0{n} ] > {cor}sair\n\n{cor}>>>{n} ').strip()[0]
+    except:
+        continue
     match opc:
         case '1':
             while True:
                 limp()
-                equacao = input(f'{n}Equação (fim para sair):\n\n{verd}').strip().lower()
+                try:
+                    equacao = input(f'{n}Equação (fim para sair):\n\n{verd}').strip().lower()
+                except:
+                    continue
+                if equacao == '': continue
                 if 'fim' in equacao: break
-                print(f'{n}{equacao} = {verd}{calcu(equacao)}{fi}')
+                print(f'{n}{equacao} = {verd}{calcu(equacao)}{f}')
                 ent()
+
         case '2':
-            print()
+            print(f'''
+    {verd}+{n} = {l}adição{f}
+    {verd}-{n} = {l}subtração{f}
+    {verd}x{n} ou {verd}*{n} = {l}multiplicação{f}
+    {verd}**{n} ou {verd}^{n} = {l}potenciação{f}
+    {verd}/{n} = {l}divisão{f}
+    {verd}//{n} = {l}divisão inteira{f} {n}({amar}o valor será a parte inteira da divisão{n})
+    {verd}%{n} = {l}resto da divisão{f} {n}({amar}o valor será o resto da divisão{n})
+    {verd}(){n} = {l}parênteses para o cálculo começar primeiro{f}''')
             ent()
-        case '3':
+
+        case '0':
             break
+limp()
+print(azul+"Até mais!"+f)
